influx6/cilium

commit e1ab352fa65634ba21802d554dac5095b8a37775

ipcache: Provide WaitForInitialSync() to wait for kvstore sync
Provide the ability for ipcache users to wait until the initial kvstore sync
has been completed.

Signed-off-by: Thomas Graf <thomas@cilium.io>

pkg/ipcache/kvstore.go
@@ -235,6 +235,7 @@ func DeleteIPFromKVStore(ip string) error {
 type IPIdentityWatcher struct {
 	backend  kvstore.BackendOperations
 	stop     chan struct{}
+	synced   chan struct{}
 	stopOnce sync.Once
 }
 
@@ -244,6 +245,7 @@ func NewIPIdentityWatcher(backend kvstore.BackendOperations) *IPIdentityWatcher 
 	watcher := &IPIdentityWatcher{
 		backend: backend,
 		stop:    make(chan struct{}),
+		synced:  make(chan struct{}),
 	}
 
 	return watcher
@@ -295,6 +297,7 @@ restart:
 					listener.OnIPIdentityCacheGC()
 				}
 				IPIdentityCache.Unlock()
+				close(iw.synced)
 
 			case kvstore.EventTypeCreate, kvstore.EventTypeModify:
 				var ipIDPair identity.IPIdentityPair
@@ -362,15 +365,29 @@ func (iw *IPIdentityWatcher) Close() {
 	})
 }
 
+func (iw *IPIdentityWatcher) waitForInitialSync() {
+	<-iw.synced
+}
+
+var (
+	watcher     *IPIdentityWatcher
+	initialized = make(chan struct{}, 0)
+)
+
 // InitIPIdentityWatcher initializes the watcher for ip-identity mapping events
 // in the key-value store.
 func InitIPIdentityWatcher() {
 	setupIPIdentityWatcher.Do(func() {
 		globalMap = newKVReferenceCounter(kvstoreImplementation{})
-		go func() {
-			log.Info("Starting IP identity watcher")
-			watch := NewIPIdentityWatcher(kvstore.Client())
-			watch.Watch()
-		}()
+		log.Info("Starting IP identity watcher")
+		watcher = NewIPIdentityWatcher(kvstore.Client())
+		close(initialized)
+		go watcher.Watch()
 	})
+}
+
+// WaitForInitialSync waits until the ipcache has been synchronized from the kvstore
+func WaitForInitialSync() {
+	<-initialized
+	watcher.waitForInitialSync()
 }

daemon/state.go
@@ -246,6 +246,7 @@ func (d *Daemon) regenerateRestoredEndpoints(state *endpointRestoreState) (resto
 				scopedLog.WithError(err).Warn("Unable to restore endpoint")
 				epRegenerated <- false
 			}
+
 			// Wait for initial identities from the kvstore before
 			// doing any policy calculation for endpoints that don't have
 			// a fixed identity or are not well known.

commit f2cd79e80574e838b72659303def3d71803c359b

pkg/ipcache: do not block initialization of identity watcher
Since identity watcher relies in the kvstore, we can't block its
initalization otherwise we can't initialize the kvstore in parallel.

Fixes: e1ab352fa656 ("ipcache: Provide WaitForInitialSync() to wait for kvstore sync")
Signed-off-by: André Martins <andre@cilium.io>

pkg/ipcache/kvstore.go
@@ -382,10 +382,12 @@ var (
 func InitIPIdentityWatcher() {
 	setupIPIdentityWatcher.Do(func() {
 		globalMap = newKVReferenceCounter(kvstoreImplementation{})
-		log.Info("Starting IP identity watcher")
-		watcher = NewIPIdentityWatcher(kvstore.Client())
-		close(initialized)
-		go watcher.Watch()
+		go func() {
+			log.Info("Starting IP identity watcher")
+			watcher = NewIPIdentityWatcher(kvstore.Client())
+			close(initialized)
+			watcher.Watch()
+		}()
 	})
 }
 